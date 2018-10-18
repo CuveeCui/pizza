3be2f5443d9c8a6403923b58a24642feb6deaf2e
CuveeCui <18280133086@163.com>
2018-10-18 14:28:47 +0800

    modify prompt style

--- meta.js
+++ meta.js
@@ -1,33 +1,35 @@
 module.exports = {
-  prompt: [
-    {
+  prompt: {
+    'project_type': {
       type: 'list',
       name: 'project_type',
       message: 'Project type',
       choices: ['SPA', 'MPA']
     },
-    {
+    'project_name': {
       type: 'string',
       name: 'project_name',
       message: `Project name`,
     },
-    {
+    'description': {
       type: 'string',
       name: 'description',
       message: 'Project description'
     },
-    {
+    'author': {
       type: 'string',
       name: 'author',
       message: `Author`
     },
-    {
+    'sentry': {
       type: 'confirm',
       name: 'sentry',
       message: 'Use sentry to your code?',
       default: true
     }
-  ],
+  }
+
+  ,
   done: () => {
 
   }
